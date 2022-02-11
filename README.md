efa367c4547d29b8fed79e1443ccdcdf0363ef06
moussaamara <moussaamara@outlook.fr>
2022-02-11 17:39:05 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-material-login-form-sd2olx
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-material-login-form-sd2olx)
---
+++ src/app/material.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+
+import {
+  MatCardModule,
+  MatInputModule,
+  MatButtonModule
+} from '@angular/material';
+
+const modules = [
+  MatCardModule,
+  MatInputModule,
+  MatButtonModule
+];
+
+@NgModule({
+  imports: modules,
+  exports: modules,
+})
+export class MaterialModule {}
